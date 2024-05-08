e6811edfba952601007ea335ddc9f68e771c918b
Oleh Salohub <so7948442@gmail.com>
2024-05-08 23:55:37 +0200

    Make push with parameters

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,20 +1,56 @@
 pipeline {
     agent any
     parameters {
-
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
         choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
-
-
     }
+    environment {
+        REPO = 'https://github.com/telest0/kbot'
+        BRANCH = 'main'
+    }
+
     stages {
-        stage('Example') {
+        stage('clone') {
             steps {
-                echo "Build for platform ${params.OS}"
-
-                echo "Build for arch: ${params.ARCH}"
-
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
             }
         }
+
+        stage('test') {
+            steps {
+                echo 'Testing started'
+                sh "make test"
+            }
+        }
+
+        stage('build') {
+            steps {
+                echo "Building binary started"
+                sh "make build -e OS=${params.OS} -e ARCH=${params.ARCH}"
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo "Building image started"
+                sh "make image -e OS=${params.OS} -e ARCH=${params.ARCH}"
+            }
+        }
+
+        stage('login to GHCR') {
+             environment {
+                GITHUB_TOKEN = credentials('github')
+            }
+            steps {
+                sh "echo $GITHUB_TOKEN_PSW | docker login ghcr.io -u $GITHUB_TOKEN_USR --password-stdin"
+            }
+        }
+        
+        stage('push image') {
+            steps {
+              sh "make push -e OS=${params.OS} -e ARCH=${params.ARCH}"
+            }
+        } 
     }
 }
